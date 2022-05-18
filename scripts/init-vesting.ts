3a5b98fdf184702ba9f858d4bba7a7e26305db5d
davidnguyen65536 <66826270+davidnguyen65536@users.noreply.github.com>
2022-05-19 07:47:38 +0800

    Update init-vesting.ts
    Premint and Schedule Vesting for different groups

--- scripts/init-vesting.ts
+++ scripts/init-vesting.ts
@@ -42,15 +42,47 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       vesting.address
     )
     
-    // Mon Jan 10 2022 12:00:00 GMT+0000
-    const startTime = '1641816000'
+    // Date and time (GMT): Saturday, January 1, 2022 1:00:00 AM
+    const startTime = '1640998800'
     await vestingContract.setStartTime(startTime)
     console.log('Scheduled vesting time', startTime)
   }
 
-  console.log('Initialize Vesting - Team')
-  await preMintTokensToContract(config.team)
-  await scheduleVestingStartTime(config.team)
+  console.log('Initialize Vesting - Founder')
+  await preMintTokensToContract(config.founder)
+  await scheduleVestingStartTime(config.founders)
+
+  console.log('Initialize Vesting - Project Dev')
+  await preMintTokensToContract(config.projectDev)
+  await scheduleVestingStartTime(config.projectDev)
+
+  console.log('Initialize Vesting - Exec Team')
+  await preMintTokensToContract(config.execTeam)
+  await scheduleVestingStartTime(config.execTeam)
+
+  console.log('Initialize Vesting - Seed Investors')
+  await preMintTokensToContract(config.seedInvestors)
+  await scheduleVestingStartTime(config.seedInvestors)
+
+  console.log('Initialize Vesting - Advisors')
+  await preMintTokensToContract(config.advisors)
+  await scheduleVestingStartTime(config.advisors)
+
+  console.log('Initialize Vesting - Strategic Investors')
+  await preMintTokensToContract(config.strategicInvestors)
+  await scheduleVestingStartTime(config.strategicInvestors)
+
+  console.log('Initialize Vesting - Public Sale')
+  await preMintTokensToContract(config.publicSale)
+  await scheduleVestingStartTime(config.publicSale)
+
+  console.log('Initialize Vesting - Eco System')
+  await preMintTokensToContract(config.ecoSystem)
+  await scheduleVestingStartTime(config.ecoSystem)
+
+  console.log('Initialize Vesting - Airdrop')
+  await preMintTokensToContract(config.airdrop)
+  await scheduleVestingStartTime(config.airdrop)
 
 }
 
